59df90a611e1980a630a92143021c97210e3b48d
Rinjee <rinjigelu39@gmail.com>
2024-04-29 18:24:32 -0600

    updated index.js, readme file created

---
+++ Develop/README.md
@@ -0,0 +1,31 @@
+# readme
+
+## Description
+a readme generator that takes in input throough the cli
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+no need
+
+## Usage
+sadasd
+
+## License
+![GitHub license](https://img.shields.io/badge/license-Apache%202.0-blue.svg)
+This project is licensed under the Apache 2.0 license.
+
+## Contributing
+pay me
+
+## Tests
+asdasd
+
+## Questions
+If you have any questions about the repo, open an issue or contact me via [email](mailto:fasfasf). Also, you can find more of my work at [sfasffas](https://github.com/sfasffas).
--- Develop/index.js
+++ Develop/index.js
@@ -4,63 +4,110 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [ 
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project',
-            name: 'title',
-        },
-        {
-            type : 'input',
-            message: 'Describe your project',
-            name: 'description',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'contents',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'installations',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'usage',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'license',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'contributions',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'tests',
-        },
-        {
-            type: 'list',
-            message: 'add table of contents',
-            name: 'questions',
-        },
-    ])
-    .then((response) =>
-    writeToFile())
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type : 'input',
+        message: 'Describe your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide installation instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How is your project used?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your project:',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'How can others contribute to your project?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What commands are required to run tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What questions do you anticipate?',
+        name: 'questions',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
+    },
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md')
+    );
+}
+
+function generateReadme(answers) {
+    return `# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+![GitHub license](https://img.shields.io/badge/license-${answers.license.replace(/ /g, '%20')}-blue.svg)
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions about the repo, open an issue or contact me via [email](mailto:${answers.email}). Also, you can find more of my work at [${answers.github}](https://github.com/${answers.github}).
+`;
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateReadme(answers);
+        writeToFile('README.md', readmeContent);
+    });
+}
+
 
 // Function call to initialize app
 init();
